f29db30aef2ff55bc8bb4fbb554254123802f8a5
Daniil <khanin_daniil@mail.ru>
2020-02-23 20:52:31 +0300

    implement example
    implement example

---
+++ example/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { SvelteSimplebar } from '../src/index'
+  const data = Array.from({ length: 50 }).map((item, index) => ({ text: `Line number: ${index + 1}`}))
+</script>
+
+<style>
+  .container {
+    height: 300px;
+  }
+</style>
+
+<div class="container">
+  <SvelteSimplebar>
+    <ul class="list">
+      {#each data as { text }}
+        <li class="list-item">{text}</li>
+      {/each}
+    </ul>
+  </SvelteSimplebar>
+</div>
