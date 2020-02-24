b62e449715c702f04577736d21fb4e90aebed6e9
Daniil Khanin <khanin_daniil@mail.ru>
2020-02-24 22:26:01 +0300

    add prettier
    implement prettier

--- example/App.svelte
+++ example/App.svelte
@@ -1,16 +1,18 @@
-<script>
-  import { SvelteSimplebar } from '../src/index'
-  const data = Array.from({ length: 50 }).map((item, index) => ({ text: `Line number: ${index + 1}`}))
-</script>
-
 <style>
   .container {
     height: 300px;
   }
 </style>
 
+<script>
+  import { SvelteSimplebar } from "../src/index";
+  const data = Array.from({ length: 50 }).map((item, index) => ({
+    text: `Line number: ${index + 1}`
+  }));
+</script>
+
 <div class="container">
-  <SvelteSimplebar>
+  <SvelteSimplebar {data}>
     <ul class="list">
       {#each data as { text }}
         <li class="list-item">{text}</li>
