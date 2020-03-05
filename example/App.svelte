2041f32e299db3038fd90890837b23778402a41b
Daniil Khanin <khanin_daniil@mail.ru>
2020-03-05 14:00:39 +0300

    implement init

--- example/App.svelte
+++ example/App.svelte
@@ -6,13 +6,16 @@
 
 <script>
   import { SvelteSimplebar } from "../src/index";
+  import { onMount } from 'svelte'
   const data = Array.from({ length: 50 }).map((item, index) => ({
     text: `Line number: ${index + 1}`
   }));
+  let svelteBar
+  const initBar = bar => console.log('svelteBar', bar)
 </script>
 
 <div class="container">
-  <SvelteSimplebar>
+  <SvelteSimplebar init={initBar}>
     <ul class="list">
       {#each data as { text }}
         <li class="list-item">{text}</li>
